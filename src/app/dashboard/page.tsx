98e497bfa9a6215d7b738e1568a207fb8927e9b0
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-10-18 12:44:54 -0300

    add dashboard route protection

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,13 @@
-export default function Dashboard(){
-    return (
-        <main>
-            TESTE
-        </main>
-    )
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    redirect("/");
+  }
+
+  return <main>TESTE</main>;
 }
