c216b9666e6bea3680738b02908c3eefd492b4f6
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-10-19 18:14:17 -0300

    add categories in dashboard header

--- src/app/dashboard/components/DashboardHeader/index.tsx
+++ src/app/dashboard/components/DashboardHeader/index.tsx
@@ -1,36 +1,35 @@
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { FiPlus } from "react-icons/fi";
+import { authOptions } from "../../../../lib/auth";
+import prismaClient from "@/lib/prisma";
 
-export function DashboardHeader() {
+export async function DashboardHeader() {
+  const session = await getServerSession(authOptions);
+
+  const categories = await prismaClient.category.findMany({
+    where: { userId: session?.user.id },
+  });
   return (
     <header className="flex justify-between h-14 bg-blue-500 mt-10 rounded shadow-2xlg">
       <nav className="flex justify-center items-center ml-3">
-        <ul className="flex font-semibold text-white gap-6">
-          <li>
-            <Link
-              href={`/dashboard/:id`}
-              className="relative underline-animation"
-            >
-              Python
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={`/dashboard/:id`}
-              className="relative underline-animation"
-            >
-              Javascript
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={`/dashboard/:id`}
-              className="relative underline-animation"
-            >
-              Go Lang
-            </Link>
-          </li>
-        </ul>
+        {categories.length > 0 && (
+          <ul className="flex font-semibold text-white gap-6">
+            {categories.map((category) => (
+              <li key={category.id}>
+                <Link href={`/dashboard/category/${category.id}`}>
+                  {category.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+        {categories.length === 0 && (
+          <p>
+            Você ainda não criou nenhum categoria, clique no ícone a direita
+            para adicionar uma nova.
+          </p>
+        )}
       </nav>
       <section className="flex justify-center items-center mr-3">
         <div className="bg-cyan-500 p-1 rounded cursor-pointer hover:bg-cyan-400/60 ease-in duration-100 shadow-2xl">
