28a430cb3cdaa681cf56c68c27bb263f94e7a0a2
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-10-19 23:45:08 -0300

    fix new categorie form component path

--- src/app/dashboard/category/new/page.tsx
+++ src/app/dashboard/category/new/page.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../../../lib/auth";
 import { redirect } from "next/navigation";
-import { NewCategoryForm } from "../Form";
+import { NewCategoryForm } from "../components/Form";
 
 export default async function NewCategoryPage() {
   const session = await getServerSession(authOptions);
