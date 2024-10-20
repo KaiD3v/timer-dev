3bf0a5b511d1aa6de31a79427f2127489d19dc27
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-10-20 15:10:46 -0300

    add patch project timer and get project api routes

--- src/app/api/v1/projects/route.ts
+++ src/app/api/v1/projects/route.ts
@@ -29,8 +29,59 @@ export async function POST(request: Request) {
   } catch (error) {
     console.error("Error while creating project:", error);
     return NextResponse.json(
-      { error: "Error while register new customer" },
+      { error: "Error while register new project" },
       { status: 501 }
+    );
+  }
+}
+
+export async function PATCH(request: Request) {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    return NextResponse.json({ error: "Unauthorized " }, { status: 401 });
+  }
+
+  const { projectId, timer } = await request.json();
+  if (!timer || !projectId) {
+    return NextResponse.json(
+      { error: "É obrigatório fornecer o tempo atualizado!" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    await prismaClient.project.update({
+      where: { id: projectId },
+      data: { timer },
+    });
+  } catch (error) {
+    console.error("Error while patch project timer:", error);
+    return NextResponse.json(
+      { error: "Error while register new project" },
+      { status: 501 }
+    );
+  }
+}
+
+export async function GET(request: Request) {
+  const { projectId } = await request.json();
+  if (!projectId) {
+    return NextResponse.json(
+      { error: "É obrigatório fornecer o id do projeto!" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const project = await prismaClient.project.findUnique({
+      where: { id: projectId },
+    });
+
+    return NextResponse.json(project);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error while get project" },
+      { status: 400 }
     );
   }
 }
