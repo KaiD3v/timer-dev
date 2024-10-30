f4a6d38f49e2290164a240d5da8a105781ed7101
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-10-30 13:06:37 -0300

    fix timer format to hours/minutes/seconds

--- src/functions/formatTime.ts
+++ src/functions/formatTime.ts
@@ -1,5 +1,10 @@
-export function formatTime(minutes: number) {
-  const hours = String(Math.floor(minutes / 60)).padStart(2, "0");
-  const mins = String(minutes % 60).padStart(2, "0");
-  return `${hours}:${mins}`;
+export function formatTime(totalSeconds: number) {
+  const hours = String(Math.floor(totalSeconds / 3600)).padStart(2, "0");
+  const minutes = String(Math.floor((totalSeconds % 3600) / 60)).padStart(
+    2,
+    "0"
+  );
+  const seconds = String(totalSeconds % 60).padStart(2, "0");
+
+  return `${hours}:${minutes}:${seconds}`;
 }
